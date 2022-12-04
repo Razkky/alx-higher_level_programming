0ac9561276c175150218affcdb3791f070965c04
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-04 09:18:54 +0100

    remove if statment

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        print("{} arguments.".format(argc))
-    else:
-        print("{} argurment:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+import sys
+argc = len(sys.argv) - 1
+if argc == 0:
+    print("{} arguments.".format(argc))
+else:
+    print("{} argurment:".format(argc))
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
