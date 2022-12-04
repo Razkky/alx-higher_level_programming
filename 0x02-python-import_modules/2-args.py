3c20e44c584c6308073e39c1f4dcf4e0ee1334f5
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-04 09:21:53 +0100

    resotore file to prev version

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-if argc == 0:
-    print("{} arguments.".format(argc))
-else:
-    print("{} argurment:".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    else:
+        print("{} argurment:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
