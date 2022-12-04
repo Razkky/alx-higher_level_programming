a4107a1846639b8f16e3bc9198ee99f64bd3e1ce
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-04 09:26:23 +0100

    fix different output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, sys.argv[argc]))
     else:
         print("{} argurment:".format(argc))
         for i in range(1, argc + 1):
