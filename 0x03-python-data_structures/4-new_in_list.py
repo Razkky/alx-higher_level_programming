3001dca23d10eddad0a1def77b6febb2fd3ad09b
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-05 21:59:18 +0100

    fix coding style

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = [x for x in my_list] 
+    new_list = [x for x in my_list]
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
