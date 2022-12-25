f1e34553fef16eb4868247fb63202133b9e1e167
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-25 01:31:55 -0800

    return max num in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    maximum = 0
+    if my_list:
+        for num in my_list:
+            if num > maximum:
+                maximum = num
+            else:
+                continue
+        return maximum
+    else:
+        return None
