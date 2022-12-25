d0e190b58ce8ac6dcba96cc46fde32e2dbab41b6
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2022-12-25 01:20:02 -0800

    fix return error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3o
 def multiple_returns(sentence):
     if sentence:
-         length = len(sentence)
-         return length, sentence[0]
+        length = len(sentence)
+        return length, sentence[0]
+    return len(sentence), None
