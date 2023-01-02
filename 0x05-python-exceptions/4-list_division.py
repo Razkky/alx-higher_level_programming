0a87c7470cfb70f204a7cf0f1b9877ad8bb7ef27
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-02 09:09:59 +0100

    fix typo error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("divisin by 0")
+            print("division by 0")
             div = 0
         except (ValueError, TypeError):
             div = 0
