908eae08eb1bed0a56b9fccae535787f797f4225
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-02 20:22:35 +0100

    use getters and setters to define a class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Define a square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize instance attribute
+
+        Args:
+            size: size of square
+            """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must b >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Get area of square"""
+        return self.__size * self.__size
