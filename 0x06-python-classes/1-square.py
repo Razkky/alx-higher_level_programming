11f3e4ba85766d825ebed3ca5bf667e684d55609
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-02 19:58:06 +0100

    initialize a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Define a class named Square """
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """ Initialize instance of a square
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
