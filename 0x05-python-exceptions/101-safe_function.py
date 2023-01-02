fa6edaa34ff1c9a97319081798d8aaba4f7e2e4c
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-02 09:33:58 +0100

    fix coding style

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         answer = fct(*args)
         return answer
-    except:
+    except (IndexError, ValueError, TypeError, ZeroDivisionError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
