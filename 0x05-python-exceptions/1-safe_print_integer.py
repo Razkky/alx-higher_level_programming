b9228489ebeb7ff3335ab7959a93f73764ea93cf
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-02 09:17:43 +0100

    remove the if block

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value:
-
-        try:
-            print("{:d}".format(value))
-            return True
-        except (ValueError, TypeError):
-            return False
-    else:
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
         return False
