3e603bdb3d60413b81c70c2d69d4c307abe9b690
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-01-03 05:34:22 +0100

    remove double spacing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,21 @@ class Rectangle:
         self.height = height
 
     @property
+    def height(self):
+        """Retrieve the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
     def width(self):
         """Retrieve value of width"""
         return self.__width
@@ -30,18 +45,3 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
-    @property
-    def height(self):
-        """Retrieve the height of a rectangle"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Set the height of a rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("height  must be an integer")
-        elif value < 0:
-            raise ValueError("height  must be >= 0")
-        else:
-            self.__height = value
