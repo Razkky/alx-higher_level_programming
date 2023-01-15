478b61529f1bdbf034c7d09acec8cac713d37dd3
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-15 23:24:43 +0100

    Define a public instance method and raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pythonn3
+""" Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+    def area(self):
+        """Define a fucntion area"""
+        raise Exception("area() is not implemented")
