68890366babbdf22cfb5ffb70a6220234bce4091
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-01-17 20:53:50 +0100

    reaload json file and change student attribute

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Define a student class"""
+
+
+class Student:
+    """ A student class that defines a student"""
+
+    def __init__(self, first_name="None", last_name="None", age=0):
+        """Initialize the student class
+        Args:
+            first_name(str): first name of the student
+            last_name(str): last name of the student
+            age: age of the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args:
+            attrs(list): list of attribute to retrieve
+        Return:
+            dict of attribute and value in attrs if attrs is not empty
+            or dict of all attributes of the object"""
+        if type(attrs) == list and all(type(item) == str for item in attrs):
+            return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attribut of student instance to json attr
+        Args:
+            json(dict): The key, values to replace student instance attr
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
