ffe10be8817bf9271a80aaabd690e60edf935e1d
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-02-15 00:47:46 +0100

    create a table and insert into it

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create a second table and insert items in it
+CREATE TABLE IF NOT EXISTS second_table (
+	id INTEGER,
+	name VARCHAR(256),
+	score INTEGER
+);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES(4, "George", 8);
