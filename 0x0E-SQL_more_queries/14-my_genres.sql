8cedcd539aa6f533493a0494128eb5950c0b1ef3
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-02-16 23:23:12 +0100

    task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- list all gebres if the show Dexter from database
+SELECT name
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
