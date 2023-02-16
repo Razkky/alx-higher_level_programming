272f18fc68eeb5df4ee874396f08fcd5a9299507
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-02-16 04:17:12 +0100

    grant privileges on user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Create a new user on a database with password authentication
-CREATE USER IF NOT EXIST 'user_0d_1' IDENTIFIED BY "user_0d_1';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+GRANT ALL PRIVIlEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
