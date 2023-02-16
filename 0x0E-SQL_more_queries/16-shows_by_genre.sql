3063e8f39de5ad4d464a3683d1e683125b4b63b3
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-02-17 00:08:49 +0100

    add left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT ts.title as title, tg.name as name
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres
 ON ts.id = tv_show_genres.show_id 
-JOIN tv_genres AS tg
+LEFT JOIN tv_genres AS tg
 ON tg.id = tv_show_genres.genre_id
 ORDER BY title
