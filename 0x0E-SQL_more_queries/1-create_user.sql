920f343a8d571793a82252f4d89065bc81c83ab9
Abdul-Razak <Muhammadjamiuabdulrazak@gmail.com>
2023-02-16 05:54:35 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a new user on a database with password authentication
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVIlEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
