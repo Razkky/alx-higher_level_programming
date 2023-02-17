51a9c510201585f03eff6175bf9e1c77120ea292
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-02-17 01:41:16 +0100

    correct typo error

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- display the average temperatures in descending order
-SELECT DISTINCT city, AVG(value) AS avg_tmp 
+SELECT DISTINCT city, AVG(value) AS avg_temp 
 FROM temperatures
 GROUP BY city
 ORDER BY avg_tmp DESC;
