d0a68eee4dccdf67998f9fba4e7cb24be82f58a2
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-13 20:50:07 +0100

    fix output error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const number = parseInt(process.argv[2]);
 if (process.argv[2] === undefined || isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log('MY number: ' + number);
+  console.log('My number: ' + number);
 }
