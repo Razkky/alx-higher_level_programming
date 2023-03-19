606de245a1c3d780c954d96955886cc38aaef114
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-19 16:11:49 +0100

    safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This scripts takes an arguement and lists
+    all states from a database
+    hbtn_0c_0_usa that matches that state using MySQLdb"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access database to get list of all states
+    that contain the searched name
+    """
+
+    con = MySQLdb.connect(host="localhost", port=3306,
+                          user=argv[1], passwd=argv[2], db=argv[3])
+    with con.cursor() as curr:
+        curr.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(argv[4]))
+        states = curr.fetchall()
+
+    if states is not None:
+        for state in states:
+            print(state)
