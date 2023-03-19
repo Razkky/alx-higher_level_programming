64b2a2ff0bcb46e45d938b5bf681bccce945ef47
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-19 18:01:37 +0100

    add if block

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,15 +3,16 @@
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
-            db=argv[3], passwd=argv[2], user=argv[1])
+                         db=argv[3], passwd=argv[2], user=argv[1])
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states \
-            ON cities.state_id=states.id \
-            ORDER BY cities.id ASC")
-    cities = cur.fetchall()
-    for city in cities:
-        print(city)
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states \
+                ON cities.state_id=states.id \
+                ORDER BY cities.id ASC")
+        cities = cur.fetchall()
+    if cities:
+        for city in cities:
+            print(city)
