189337688d90e5d4f5e1e8291eaaba00a616afb0
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-19 15:38:25 +0100

    seaarch for states that starts with "N"

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This scripts lists all states from a database
+    hbtn_0c_0_usa that starts with 'N' using MySQLdb"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access database to get list of all states
+    that starts with 'N'
+    """
+
+    con = MySQLdb.connect(host="localhost", port=3306,
+                          user=argv[1], passwd=argv[2], db=argv[3])
+    curr = con.cursor()
+    curr.execute("SELECT * FROM states \
+                WHERE name LIKE 'N%'")
+    states = curr.fetchall()
+
+    for state in states:
+        print(state)
