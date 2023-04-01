d5866123017f746d991f76830634d5e65c15fbc5
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-01 12:13:56 +0100

    fix code style and typo error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,7 @@ import urllib.request as ur
 if __name__ == "__main__":
     with ur.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
-        print("Body response")
+        print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
         print(f"\t- utf8 content: {html.decode('utf-8')}")
-
